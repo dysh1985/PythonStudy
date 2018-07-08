401a537beaaee6ecef3f054c3ca27f6b5f7e7817
Youshun.Ding <283837988@qq.com>
2018-07-08 22:03:42 +0800

    modify list compr 3

--- do_listcompr.py
+++ do_listcompr.py
@@ -10,3 +10,7 @@ print([k + '=' + v for k, v in d.items()])
 
 L = ['Hello', 'World', 'IBM', 'Apple']
 print([s.lower() for s in L])
+
+#use isinstance() to filter out non-string member from the list
+L1 = ['Hello', 'World', 18, 'Apple', None]
+print([s.lower() for s in L1 if isinstance(s,str)])
